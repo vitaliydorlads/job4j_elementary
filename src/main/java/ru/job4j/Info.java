21ae782aecadff18f80178a30867e377d2d74afb
виталийдорладс <vitaliydorlads@gmail.com>
2023-04-10 12:37:36 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("10.04.2023");
     }
 }
