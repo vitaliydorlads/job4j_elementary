d7a9211dd05e3008ce949f18473af95769195de0
виталийдорладс <vitaliydorlads@gmail.com>
2023-04-20 00:18:51 +0300

    Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -15,9 +15,10 @@ public class MathFunc {
     }
 
     public static void main(String[] args) {
-        int result1 = MathFunc.func1(100);
+        int result1 = MathFunc.func1(3);
         int result2 = MathFunc.func2(5);
-        int result3 = result1 + result2;
-        System.out.println(result3);
+        int result3 = MathFunc.func1(100);
+        int total = result1 + result2;
+        System.out.println(total);
     }
 }
