5e7453a5e278d3b20608f654bc92f9624348b624
виталийдорладс <vitaliydorlads@gmail.com>
2023-04-22 20:07:08 +0300

    Аргументы [#295521 #410936]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -1,6 +1,5 @@
-package ru.job4j.calculator;
-
-public class ArgMethod {
+    package ru.job4j.calculator;
+    public class ArgMethod {
 
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
