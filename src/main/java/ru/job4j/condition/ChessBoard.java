5d89800ed4680d8d7569883d52dc17ef04475b35
виталийдорладс <vitaliydorlads@gmail.com>
2023-05-26 16:57:03 +0300

    Math.abs [#373616]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -6,9 +6,9 @@ public class ChessBoard {
         int rsl = 0;
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
-            if (x1 - x2 == y1 - y2) {
-                rsl = rsl == 0 ? Math.abs(y2 - y1) : Math.abs(x2 - x1);
-        }
+            if (Math.abs(y2 - y1) == Math.abs(x2 - x1)) {
+                rsl = Math.abs(y2 - y1);
+            }
         }
         return rsl;
     }
@@ -18,7 +18,16 @@ public class ChessBoard {
     }
 
     public static void main(String[] args) {
-        int rsl = way(2, 6, 4, 1);
-        System.out.println(rsl);
+        int rsl1 = way(6, 7, 1, 2);
+        int rsl2 = way(7, 0, 0, 7);
+        int rsl3 = way(2, 6, 4, 1);
+        int rsl4 = way(-1, 6, 4, 1);
+        int rsl5 = way(2, -1, 4, 1);
+        int rsl6 = way(2, 6, -1, 1);
+        int rsl7 = way(2, 6, 4, -1);
+        int rsl8 = way(10, 6, 4, 1);
+        int rsl9 = way(2, 10, 4, 1);
+        int rsl10 = way(2, 6, 10, 1);
+        int rsl11 = way(2, 6, 4, 10);
     }
 }
