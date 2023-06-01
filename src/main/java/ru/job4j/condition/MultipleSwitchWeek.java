37dd78deafa39946a8a7a2e21f15c4ef50c7caee
виталийдорладс <vitaliydorlads@gmail.com>
2023-06-01 19:58:26 +0300

    Цикл for. Определение. [#3572]

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 public class MultipleSwitchWeek {
+
     public static int numberOfDay(String name) {
         return switch (name) {
             case "Понедельник", "Monday" -> 1;
