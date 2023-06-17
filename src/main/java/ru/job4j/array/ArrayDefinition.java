77fe010a332c32d19af3b7c2a77faf2625e791ca
виталийдорладс <vitaliydorlads@gmail.com>
2023-06-17 22:04:55 +0300

    Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("размер ages " + ages.length);
+        System.out.println("размер surnames " + surnames.length);
+        System.out.println("размер prices " + prices.length);
     }
 }
