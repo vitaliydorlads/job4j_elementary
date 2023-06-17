5692edeb819c21cb861c91307ee83892b58f1ab6
виталийдорладс <vitaliydorlads@gmail.com>
2023-06-17 22:01:00 +0300

    Объявление массива. [#3613]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,7 +1,9 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    short[] ages = new short[10];
-    String[] surnames = new String[100500];
-    float[] prices = new float[40];
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
 }
