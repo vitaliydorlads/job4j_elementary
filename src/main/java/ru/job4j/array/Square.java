2ec9b5ece78b432d49d5e9cbe98385d99423508f
виталийдорладс <vitaliydorlads@gmail.com>
2023-06-20 19:07:32 +0300

    Массивы в тестах. [#226789]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -10,7 +10,7 @@ public class Square {
     }
 
     public static void main(String[] args) {
-        int[] array = calculate(4);
+        int[] array = calculate(5);
         for (int index : array) {
             System.out.print(index + " ");
         }
---
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SquareTest {
+
+    @Test
+    public void whenBound5Then014916() {
+        int bound = 5;
+        int[] result = Square.calculate(bound);
+        int[] expected = new int[]{0, 1, 4, 9, 16};
+        assertThat(result).containsExactly(expected);
+    }
+}
