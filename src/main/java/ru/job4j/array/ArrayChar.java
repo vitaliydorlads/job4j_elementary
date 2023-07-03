8b423e1cadf5727612a30106f3ab2a6a4902df08
виталийдорладс <vitaliydorlads@gmail.com>
2023-07-03 19:39:48 +0300

    Массив заполнен true или false [#53857 #426528]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i2]) {
+                    result = !result;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,9 +3,10 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 1; i < data.length; i++) {
             if (data[i] != data[0]) {
-                result = !result;
+                result = false;
+                break;
             }
         }
         return result;
