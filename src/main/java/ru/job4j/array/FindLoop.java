d13966f3edb7b774b0890211886c66c2e3b74202
виталийдорладс <vitaliydorlads@gmail.com>
2023-07-07 21:18:10 +0300

    Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,13 +1,21 @@
 package ru.job4j.array;
 
 public class FindLoop {
-
     public static int indexOf(int[] data, int el) {
         int rst = -1;
-        for (int index = 0; index < data.length; index++) {
-            if (data[index] == el) {
-                rst = index;
-                break;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                return i;
+            }
+        }
+        return rst;
+    }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int x = start; x <= finish; x++) {
+            if (data[x] == el) {
+                return x;
             }
         }
         return rst;
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -8,7 +8,7 @@ class FindLoopTest {
 
     @Test
     public void whenArrayHas5Then0() {
-        int[] data = new int[]{5, 10, 3};
+        int[] data = new int[] {5, 10, 3};
         int el = 5;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
@@ -16,11 +16,41 @@ class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHasNot10ThenMinus1() {
-        int[] data = new int[]{5, 9, 3};
-        int el = 10;
-        int result = FindLoop.indexOf(data, el);
-        int expected = -1;
+    public void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int el = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 5;
         assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int el = 4;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        /* Дописать тест, когда элемент 8 в диапазоне не найден.
+        Можно искать любой другой элемент, корректно отражая этот факт в названии теста. */
     }
 }
