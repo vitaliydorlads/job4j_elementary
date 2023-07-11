707827495dbd3cf07c28f9b56cba38132d434369
виталийдорладс <vitaliydorlads@gmail.com>
2023-07-11 20:01:47 +0300

    Поиск индекса в диапазоне

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -2,22 +2,20 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        int rst = -1;
         for (int i = 0; i < data.length; i++) {
             if (data[i] == el) {
                 return i;
             }
         }
-        return rst;
+        return -1;
     }
 
     public static int indexInRange(int[] data, int el, int start, int finish) {
-        int rst = -1;
         for (int x = start; x <= finish; x++) {
             if (data[x] == el) {
                 return x;
             }
         }
-        return rst;
+        return -1;
     }
 }
