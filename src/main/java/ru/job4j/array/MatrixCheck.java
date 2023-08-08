fb46dd869f086fb62a554c0a5a4e66c0de310f93
виталийдорладс <vitaliydorlads@gmail.com>
2023-08-08 19:42:18 +0300

    Моно столбец в матрице + Массив из диагонали матрицы

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -11,4 +11,23 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int i = 0; i < rsl.length; i++) {
+            rsl[i] = board[i][i];
+        }
+        return rsl;
+    }
 }
