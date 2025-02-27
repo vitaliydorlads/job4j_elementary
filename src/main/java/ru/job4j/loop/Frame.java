6420e171e33d0291dba59d60c58c0e73fe1a6fdb
Vitaliy <vitaliydorlads@gmail.com>
2025-02-27 21:12:26 +0300

    Рисование рамки в консоли [#505217]

---
+++ src/main/java/ru/job4j/loop/Frame.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+public class Frame {
+
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                boolean isTopOrBottomBorder = (row == 0 || row == size - 1);
+                boolean isSideBorder = (col == 0 || col == size - 1);
+                 if (isTopOrBottomBorder && isSideBorder) {
+                    System.out.print("+");
+                } else if (isTopOrBottomBorder) {
+                    System.out.print("-");
+                } else if (isSideBorder) {
+                    System.out.print("|");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw frame of size 3");
+        draw(3);
+        System.out.println("Draw frame of size 5");
+        draw(5);
+    }
+}
+
+
