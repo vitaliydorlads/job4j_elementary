4682426419bdb55db4d8911f7f7ef37e614169ad
Vitaliy <vitaliydorlads@gmail.com>
2025-02-28 21:06:17 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Vitaliy";
+        names[1] = "Alexandrovich";
+        names[2] = "16.02.1991";
+        names[3] = "Russia";
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println(names[0] + " " + names[1] + "," + names[2] + "," + names[3]);
     }
 }
