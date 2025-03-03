54b54e83203f39da4e3e1dee7499df1738b50bf5
Vitaliy <vitaliydorlads@gmail.com>
2025-03-03 19:28:22 +0300

    Классический поиск перебором [#505227]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int element) {
-        int result = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        int result = -1;
         for (int index = 0; index < data.length; index++) {
             if (data[index] == element) {
                 result = index;
