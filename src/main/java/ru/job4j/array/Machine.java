8a5c90b0c87606462e13dc84cad09b4781570b2b
Vitaliy <vitaliydorlads@gmail.com>
2025-03-12 22:21:53 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#505250]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,10 +7,10 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] result = new int[100];
         int size = 0;
-        for (int i = 0; i < coins.length; i++) {
-            while ((money - price) >= coins[i]) {
-                money -= coins[i];
-                result[size] = coins[i];
+        for (int coin : coins) {
+            while ((money - price) >= coin) {
+                money -= coin;
+                result[size] = coin;
                 size++;
             }
         }
