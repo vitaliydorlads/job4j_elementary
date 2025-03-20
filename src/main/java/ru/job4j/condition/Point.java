7695b230f0525ee00b7e6fa42827e3e6ce56c561
Vitaliy <vitaliydorlads@gmail.com>
2025-03-20 21:07:05 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587 #468099]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,16 +1,22 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    private int x;
+    private int y;
+
+    Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        double resul2 = distance(1, 1, 1, 1);
-        System.out.println("result (1, 1) to (1, 1) " + resul2);
-        double resul3 = distance(-2, -2, 2, 2);
-        System.out.println("result (-2, -2) to (2, 2) " + resul3);
+        Point a = new Point(-2, -2);
+        Point b = new Point(2, 2);
+        double distance = a.distance(b);
+        System.out.println(distance);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,26 +7,32 @@ import static org.assertj.core.api.Assertions.*;
 class PointTest {
 
     @Test
-    void whenPoints00And20Then2() {
-        int x1 = 0, y1 = 0, x2 = 2, y2 = 0;
+    void whenPoints00And02Then2() {
+        int x1 = 0, y1 = 0;
+        int x2 = 0, y2 = 2;
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
         double expected = 2.0;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        assertThat(a.distance(b)).isEqualTo(expected);
     }
 
     @Test
-    void whenPointsMinus1Minus1And1And1Then2Dot83() {
-        int x1 = 1, y1 = 1, x2 = 1, y2 = 1;
-        double expected = 0.0;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    void whenPoints12And02Then1() {
+        int x1 = 1, y1 = 2;
+        int x2 = 0, y2 = 2;
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
+        double expected = 1.0;
+        assertThat(a.distance(b)).isEqualTo(expected);
     }
 
     @Test
-    void whenPointsMinus2Minus2And2And2Then5Dot66() {
-        int x1 = -2, y1 = -2, x2 = 2, y2 = 2;
+    void whenPointsMinus2Minus2And22Then5Dot66() {
+        int x1 = -2, y1 = -2;
+        int x2 = 2, y2 = 2;
+        Point a = new Point(x1, y1);
+        Point b = new Point(x2, y2);
         double expected = 5.66;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+        assertThat(a.distance(b)).isEqualTo(expected, withPrecision(0.01));
     }
 }
